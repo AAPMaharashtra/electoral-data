bcf3be25cd82622fa097d5aad6213cd2dbd78069
Neehar Venugopal <neeharv@gmail.com>
2014-01-22 19:44:38 +0530

    Converted all URLs to relative reversed URLs.

--- electoral_data/views.py
+++ electoral_data/views.py
@@ -1,48 +1,48 @@
 from django.shortcuts import render, redirect
 from electoral_data.models import LokSabhaSeat,AssemblyConstituency,PollingStation,Society,Citizen,CitizenInterestForm, SocietyProcessedForm
 from django.contrib.auth.decorators import login_required
-
+from django.core.urlresolvers import reverse
 
 # Create your views here.
-@login_required(login_url='/login')
+@login_required()
 def index(request):
 	if request.user.is_staff:
-		return redirect("/admin/")
+		return redirect(reverse('admin:index'))
 	else:		
-		return redirect("/societies/"+str(request.user.get_profile().polling_station.id))
+		return redirect(reverse('electoral_data.views.societies',args=[str(request.user.get_profile().polling_station.id)]))
 
 # Show LS seats
-@login_required(login_url='/login')
+@login_required()
 def ls(request):
 	if request.user.is_staff:
 		ls_seats_list = LokSabhaSeat.objects.all()
 		context = {'ls_seats_list': ls_seats_list}
 		return render(request, 'electoral_data/ls.html', context)
 	else:
-		return redirect("/societies/"+str(request.user.get_profile().polling_station.id))
+		return redirect(reverse('electoral_data.views.societies',args=[str(request.user.get_profile().polling_station.id)]))
 	
 # Show Assembly seats for LS
-@login_required(login_url='/login')
+@login_required()
 def assembly(request,ls_id):
 	if request.user.is_staff:
 		assembly_list = AssemblyConstituency.objects.filter(lok_sabha_seat=ls_id)
 		context = {'assembly_list': assembly_list}
 		return render(request, 'electoral_data/assembly.html', context)
 	else:
-		return redirect("/societies/"+str(request.user.get_profile().polling_station.id))
+		return redirect(reverse('electoral_data.views.societies',args=[str(request.user.get_profile().polling_station.id)]))
 
 # Show Polling booths for the Assembly seat
-@login_required(login_url='/login')
+@login_required()
 def polling(request,assembly_id):
 	if request.user.is_staff:
 		polling_list = PollingStation.objects.filter(constituency=assembly_id)
 		context = {'polling_list': polling_list}
 		return render(request, 'electoral_data/polling.html', context)
 	else:
-		return redirect("/societies/"+str(request.user.get_profile().polling_station.id))
+		return redirect(reverse('electoral_data.views.societies',args=[str(request.user.get_profile().polling_station.id)]))
 
 # Show societies for the Polling Station
-@login_required(login_url='/login')
+@login_required()
 def societies(request,polling_id):
 	societies_list = Society.objects.filter(polling_station=polling_id)
 	polling_station = PollingStation.objects.get(id=polling_id)
@@ -50,7 +50,7 @@ def societies(request,polling_id):
 	return render(request, 'electoral_data/society.html', context)
 
 # Show Citizens in the Society
-@login_required(login_url='/login')
+@login_required()
 def citizens(request,society_id):
 	citizens_list = Citizen.objects.filter(society=society_id)
 	if request.method == 'POST':
@@ -58,7 +58,7 @@ def citizens(request,society_id):
 		form = SocietyProcessedForm(request.POST, instance=society)
 		form.save()
 		polling_id = society.polling_station.id
-		return redirect('/societies/'+str(polling_id))
+		return redirect(reverse('electoral_data.views.societies',args=[str(polling_id)]))
 	else:
 		form = SocietyProcessedForm(instance=citizens_list[0])
 		context = {'citizens_list': citizens_list,'society_id': society_id,'form': form}
@@ -66,14 +66,14 @@ def citizens(request,society_id):
 
 
 # Show Citizen details
-@login_required(login_url='/login')
+@login_required()
 def detail(request,citizen_id):
 	citizen_details = Citizen.objects.filter(id=citizen_id)
 	if request.method == 'POST':
 		form = CitizenInterestForm(request.POST, instance=citizen_details[0])
 		form.save()
 		society_id = citizen_details[0].society.society_no
-		return redirect('/citizens/'+society_id)
+		return redirect(reverse('electoral_data.views.citizens',args=[society_id]))
 	else:
 		form = CitizenInterestForm(instance=citizen_details[0])
 		context = {'citizen_details': citizen_details,'form': form}
